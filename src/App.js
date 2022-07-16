9a8d93e0b47147f6529d4931e71c0ac977e126b6
ZekarioRPL <sefdannynur12@gmail.com>
2022-07-16 17:48:12 +0700

    baru

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';
+// import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+// export default App;
+// # defauld
+
+import React, { Component } from "react"
+
+class App extends Component {
+  constructor() {
+    super() 
+
+      this.state = {
+        name : 'Sef Danny'
+      }
+    
+  }
+
+  render() {
+    return (
+    <div>
+      <div>Hello</div>
+      <div>My name is {this.state.name}</div>
     </div>
-  );
+    )
+  }
 }
 
 export default App;
